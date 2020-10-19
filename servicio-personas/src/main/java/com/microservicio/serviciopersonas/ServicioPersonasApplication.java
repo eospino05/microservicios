c4109d89c3ca6a5dcef568fc9ef18119df9b3396
Elder Ospino <elderospino@gmail.com>
2020-10-18 20:44:34 -0500

    Proyectos iniciales

---
+++ servicio-personas/src/main/java/com/microservicio/serviciopersonas/ServicioPersonasApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.serviciopersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioPersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioPersonasApplication.class, args);
+	}
+
+}
---
+++ servicio-personas/src/test/java/com/microservicio/serviciopersonas/ServicioPersonasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.serviciopersonas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioPersonasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
