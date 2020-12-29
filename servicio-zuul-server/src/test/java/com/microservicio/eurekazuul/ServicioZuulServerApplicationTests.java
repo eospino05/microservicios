8cb2c1bc81568c2dc71f0337f7055cde2f8d8772
Elder Ospino <elderospino@gmail.com>
2020-12-28 22:06:25 -0500

    Servidor Zuul inicial que funcionará como Gatewey

---
+++ servicio-zuul-server/src/test/java/com/microservicio/eurekazuul/ServicioZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.eurekazuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
