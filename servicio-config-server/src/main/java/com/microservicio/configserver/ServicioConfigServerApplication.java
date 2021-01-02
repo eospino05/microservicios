d48d67e70b4f7fe1cc408900f9622737a5fe94ef
Elder Ospino <elderospino@gmail.com>
2021-01-02 11:42:24 -0500

    Se crea proyecto para configuración centralizada config-server

---
+++ servicio-config-server/src/main/java/com/microservicio/configserver/ServicioConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioConfigServerApplication.class, args);
+	}
+
+}
