5e48d75f5dbf294776b631aada7318767a9ba16d
Elder Ospino <elderospino@gmail.com>
2021-01-02 21:12:20 -0500

    - Implementación config-server centralizada en git - Configuración para ambientes dev y prod - Creación de repositorio que contiene archivos de configuracion .properties - Configuracion para actualizar modificacione de propiedades personalizadas mediante endpoint http://localhost:8005/actuator/refresh

--- servicio-cliente/src/main/java/com/microservicio/serviciocliente/controllers/ClienteController.java
+++ servicio-cliente/src/main/java/com/microservicio/serviciocliente/controllers/ClienteController.java
@@ -1,9 +1,14 @@
 package com.microservicio.serviciocliente.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,14 +21,22 @@ import com.microservicio.serviciocliente.model.Persona;
 import com.microservicio.serviciocliente.model.services.ClienteServicio;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 //@RequestMapping("/api/cliente/personas")
+
 public class ClienteController {
 
 	@Autowired
 	//@Qualifier("servicioRestTemplate")
 	@Qualifier("servicioFeing")
 	ClienteServicio clienteServicio;
+	
+	@Autowired
+	private Environment env;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	 @GetMapping
 	    public ResponseEntity<List<Grupo>> list(){
@@ -58,6 +71,23 @@ public class ClienteController {
 	    	Grupo g = new Grupo(p, grupo);
 	    	
 	    	return new ResponseEntity(g, HttpStatus.OK);
+	    }
+	    
+	    @GetMapping("/obtener-config")
+	    public ResponseEntity<?> load(@Value("${server.port}") String puerto){
+	    	
+	    	Map<String, String> json = new HashMap<String, String>();
+	    	json.put("texto", texto);
+	    	json.put("puerto", puerto);
+	    	
+	    	if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+	    		json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+	    		json.put("autor.email", env.getProperty("configuracion.autor.email"));
+	    	}
+	    	
+	    	return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	          
+	       
 	    }
 	    
 	    
